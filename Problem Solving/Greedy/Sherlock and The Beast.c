85bd2d8a1f9d65b4675278f70b89b69399344ebd
ngocmunghy <38464875+ngocmunghy@users.noreply.github.com>
2021-02-23 13:42:26 +0700

    Create Sherlock and The Beast.c

---
+++ Problem Solving/Greedy/Sherlock and The Beast.c
@@ -0,0 +1,154 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+// Complete the decentNumber function below.
+void decentNumber(int n) {
+    if(n < 3) {
+        printf("-1");
+    } else if(n % 3 == 0) {
+        for(int i=0;i<n;i++) {
+            printf("5");
+        }
+    } else {
+        int a = 5;
+        while(n-a > 0) {
+            if((n-a) % 3 != 0)  {
+                a += 5;
+            } else {
+                break;
+            }
+        }
+        if(n-a >= 0) {
+            for(int i=0;i<n-a;i++) {
+                printf("5");
+            }
+            for(int i=0;i<a;i++) {
+                printf("3");
+            }
+        } else {
+            printf("-1");
+        }
+    }
+    printf("\n");
+    
+}
+
+int main()
+{
+    char* t_endptr;
+    char* t_str = ltrim(rtrim(readline()));
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char* n_endptr;
+        char* n_str = ltrim(rtrim(readline()));
+        int n = strtol(n_str, &n_endptr, 10);
+
+        if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        decentNumber(n);
+    }
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
