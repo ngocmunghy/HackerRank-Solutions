64c72f5d3e2a862fd373d4f97ad1da63dd2446de
Ngoc Mung <38464875+ngocmunghy@users.noreply.github.com>
2021-03-21 18:30:44 +0700

    Create Arrays - DS.c

---
+++ Data Structures/Array/Arrays - DS.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int i,n;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++) {
+        scanf("%d ",&a[i]);
+    }
+    for(i=0;i<n;i++) {
+        printf("%d ",a[n-i-1]);
+    }
+    return 0;
+}
