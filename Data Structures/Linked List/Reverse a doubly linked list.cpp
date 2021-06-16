fd0614652a5b40953dc19dcf099f2486ac77c77e
Ngoc Mung <38464875+ngocmunghy@users.noreply.github.com>
2021-06-16 22:43:32 +0700

    Create Reverse a doubly linked list.cpp

---
+++ Data Structures/Linked List/Reverse a doubly linked list.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*
+ * Complete the 'reverse' function below.
+ *
+ * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+ * The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+
+// 1-2-3    -> 3-2-1
+// 1-2-3-4  -> 4-3-2-1
+
+DoublyLinkedListNode* reverse(DoublyLinkedListNode* llist) {
+    DoublyLinkedListNode * head = llist;
+    DoublyLinkedListNode * tail = llist;
+    int nodeNum = 1;
+    while(tail->next != NULL) {
+        tail = tail->next;
+        nodeNum++;
+    }
+    
+    int start = 1, end = nodeNum, tmp;
+    while(start < end) {
+        tmp = head->data;
+        head->data = tail->data;
+        tail->data = tmp;
+        start++;
+        end--;
+        head = head->next;
+        tail = tail->prev;
+    }
+    return llist;
+}
+
+int main()
