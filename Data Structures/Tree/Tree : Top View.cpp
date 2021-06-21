f0be510ddeb6aa0c0839f408e6f4f6bd4114c99e
Ngoc Mung <38464875+ngocmunghy@users.noreply.github.com>
2021-06-21 17:52:33 +0700

    Create Tree : Top View.cpp

---
+++ Data Structures/Tree/Tree : Top View.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void topView(Node * root) {
+        
+        queue<pair<Node*,int>> q;
+        map<int,int> m;
+        q.push(make_pair(root,0));
+        
+        while(!q.empty()) {
+            pair<Node*,int> front = q.front();
+            q.pop();
+            int hd = front.second;
+            Node* temp = front.first;
+            if(m.find(hd) == m.end()) {
+                m[hd] = temp->data;
+            }
+            if(temp->left) {
+                q.push(make_pair(temp->left, hd - 1));
+            }
+            if(temp->right) {
+                q.push(make_pair(temp->right, hd + 1));
+            }
+        }
+        
+        for(auto x:m) {
+            cout<< x.second<< " ";
+        }
+    }
+
+}; //End of Solution
